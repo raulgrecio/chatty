a0bffdd2f33a315a57217456cd0b1089cde5512f
Simon Tucker <srtucker22@gmail.com>
2017-02-20 19:10:49 -0600

    cleaner app component

--- client/components/app.component.js
+++ client/components/app.component.js
@@ -1,7 +1,4 @@
 import React, { Component } from 'react';
-import {
-  StyleSheet,
-} from 'react-native';
 import { ApolloProvider } from 'react-apollo';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
@@ -24,25 +21,6 @@ const store = createStore(
     applyMiddleware(client.middleware()),
   ),
 );
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 export default class App extends Component {
   render() {
