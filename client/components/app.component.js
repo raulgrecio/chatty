f4fb732147abc94d2a014695a675bc71a726ac94
Simon Tucker <srtucker22@gmail.com>
2017-04-09 17:52:41 +0900

    removed step-03 stuff and upgraded packages

--- client/components/app.component.js
+++ client/components/app.component.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 import { ApolloProvider } from 'react-apollo';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import ApolloClient, { createNetworkInterface } from 'apollo-client';
-
-import { Routes, Scenes } from './routes.component';
 
 const networkInterface = createNetworkInterface({ uri: 'http://localhost:8080/graphql' });
 
@@ -22,11 +25,41 @@ const store = createStore(
   ),
 );
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
 export default class App extends Component {
   render() {
     return (
       <ApolloProvider store={store} client={client}>
-        <Routes scenes={Scenes} />
+        <View style={styles.container}>
+          <Text style={styles.welcome}>
+            Welcome to React Native!
+          </Text>
+          <Text style={styles.instructions}>
+            To get started, edit index.ios.js
+          </Text>
+          <Text style={styles.instructions}>
+            Press Cmd+R to reload,{'\n'}
+            Cmd+D or shake for dev menu
+          </Text>
+        </View>
       </ApolloProvider>
     );
   }
